a538683d460450dd5b3ed4596a8bce3e61a53b4f
david.c.liebman <david.c.liebman@9563fa94-8a37-d0f4-0ffc-d383d6e30373>
2010-09-05 11:53:54 +0000

    working on congrats

--- trunk/src/org/davidliebman/android/awesomeguy/Congrats.java
+++ trunk/src/org/davidliebman/android/awesomeguy/Congrats.java
@@ -1,7 +1,9 @@
 package org.davidliebman.android.awesomeguy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class Congrats extends Activity {
 
@@ -11,4 +13,11 @@ public class Congrats extends Activity {
         setContentView(R.layout.congrats);      
 
 	}
+	
+	@Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+        	startActivity(new Intent("org.davidliebman.android.awesomeguy.GameStart"));        }
+        return true;
+    }
 }
