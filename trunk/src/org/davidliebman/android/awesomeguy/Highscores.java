40e345360fec060193293b36866af889ed4f096f
david.c.liebman <david.c.liebman@9563fa94-8a37-d0f4-0ffc-d383d6e30373>
2010-11-10 13:23:56 +0000

    viewable highscores page, but no database support yet

--- trunk/src/org/davidliebman/android/awesomeguy/Highscores.java
+++ trunk/src/org/davidliebman/android/awesomeguy/Highscores.java
@@ -1,15 +1,31 @@
 package org.davidliebman.android.awesomeguy;
 
 import android.app.ListActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
-public class Highscores extends ListActivity {
+public class Highscores   extends Activity { //ListActivity {
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.highscores);      
 
+        
+        /* button at bottom of view */
+        final Button button = (Button) findViewById(R.id.button_highscores);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	Intent StartGameIntent = new Intent(Highscores.this,GameStart.class);
+        		startActivity(StartGameIntent);
+            	//Toast.makeText(Players.this, "And We're Off", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
 	}
 }
