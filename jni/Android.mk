855920dd9964451c8893444641654c1266999eb2
david.c.liebman <david.c.liebman@9563fa94-8a37-d0f4-0ffc-d383d6e30373>
2012-05-01 15:52:11 +0000

    jni still

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,8 +4,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := awesomeguy
-LOCAL_CFLAGS    := -Werror -D GL_GLEXT_PROTOTYPES
+LOCAL_CFLAGS    := -Werror -DGL_GLEXT_PROTOTYPES
 LOCAL_SRC_FILES := awesomeguy.c androidgl.c
-LOCAL_LDLIBS    := -llog -lGLESv1_CM -ldl -llog
+LOCAL_LDLIBS    := -llog -lGLESv1_CM 
 
 include $(BUILD_SHARED_LIBRARY)
--- jni/androidgl.c
+++ jni/androidgl.c
@@ -16,20 +16,15 @@ void init(void)
 
     tex_width = TEX_DIMENSION;
     tex_height = TEX_DIMENSION;
-    //otick = 0;
 
-    if (!pixbuf) {
-	pixbuf = malloc(tex_width * tex_height * 2);
-	//copy_to_texture();
-	//uint8_t buffer[tex_height * tex_width * 2];
-	//pixbuf = buffer;
-	assert(pixbuf);
-        //memcpy(pixbuf, default_image, VIDEO_WIDTH*VIDEO_HEIGHT*2);
-	//copy_to_texture();
-    }
+	if (!pixbuf) {
+		pixbuf = malloc(tex_width * tex_height * 2);
+	
+		assert(pixbuf);
+        
+	}
 
 	glEnable(GL_TEXTURE_2D);
-	//glTexEnvx(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 	
 	//glDeleteTextures(1, &texture);
 	glGenTextures(1, &texture);
@@ -41,46 +36,41 @@ void init(void)
 	glTexParameterf( GL_TEXTURE_2D,
 			GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	glShadeModel( GL_FLAT );
+	glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_CROP_RECT_OES, crop);
 
-    // Call glTexImage2D only once, and use glTexSubImage2D later
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, 
+	glShadeModel(GL_FLAT);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, 
+		GL_RGB, 
+		tex_width, tex_height, 
+		0, 
 		GL_RGB,
-		//GL_UNSIGNED_SHORT_5_5_5_1,
 		GL_UNSIGNED_SHORT_5_6_5, 
 		NULL);
 
 
-	glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_CROP_RECT_OES, crop);
-
-    //    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT,
-    //                                    GL_RGB, GL_UNSIGNED_SHORT_5_6_5, default_image);
-
-	//glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	//glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	//glTexEnvx(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	//glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
 
 
 	glDisable(GL_BLEND);
 	glDisable(GL_DITHER);
 	glDisable(GL_DEPTH_TEST);
-
+	glDisable(GL_FOG);
+	glDisable(GL_LIGHTING);
+	glDisable(GL_ALPHA_TEST);
+	glDisable(GL_COLOR_LOGIC_OP);
+	glDisable(GL_COLOR_MATERIAL);
+	glDisable(GL_STENCIL_TEST);
 
 	glDepthMask(GL_FALSE);
 	glDisable(GL_CULL_FACE);
 
 
-	glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-	//glColor4x(0xfffff, 0x10000, 0x10000, 0x10000);
+	//glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
 
-	glShadeModel(GL_FLAT);
+	//glClear(GL_COLOR_BUFFER_BIT);
 
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	//glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE );
 }
 
 void copy_to_texture() {
@@ -96,15 +86,13 @@ void copy_to_texture() {
 	//		pixbuf[( i * tex_height * 2 ) + ( j * 2 ) + 1] = 0xff;
 	//	}
 	//}
-	//glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
 
-	//glClear(GL_COLOR_BUFFER_BIT);	//test
 	pthread_mutex_t lock;
 	pthread_mutex_init(&lock, NULL);
 
 	pthread_mutex_lock(&lock);
 	for (i = 0; i < TEX_DIMENSION * TEX_DIMENSION ; i ++ ) {
-		pixbuf[i] = RGB565(0xf,0xf,0xf);
+		pixbuf[i] = RGB565(31,31,0);
 
 	}
 
@@ -117,9 +105,11 @@ void copy_to_texture() {
 
 	glDrawTexiOES(0, 0, 0, screen_width, screen_height);
 	if (glGetError() != GL_NO_ERROR) exit(3);
-	LOGE("2: code");
+	//LOGE("2: code");
 	pthread_mutex_unlock(&lock);	
-
+	
+	glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);	//test
 
 	//glBindTexture(GL_TEXTURE_2D, 0);
 	//glBindTexture(GL_TEXTURE_2D, texture);
