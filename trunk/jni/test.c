8ca350c06a0a4c707bc59a053751c27e7b082769
david.c.liebman <david.c.liebman@9563fa94-8a37-d0f4-0ffc-d383d6e30373>
2012-05-15 15:58:33 +0000

    test library

--- trunk/jni/test.c
+++ trunk/jni/test.c
@@ -1,12 +1,11 @@
 #include "androidgl.h"
 
 /**
- *	Used to notify JNI of android sdk version.
+ *	Used to test JNI
  *
  *	@param	env				required by all java jni
  *	@param	obj				required by all java jni
- *	@param	v				sdk version
- *	@return					void
+ *	@return					score
  */
 JNIEXPORT jint JNICALL Java_org_davidliebman_android_awesomeguy_Panel_JNItestScore(JNIEnv * env, jobject  obj)
 {
