6699c35439aa7e35b4deb0cb1e2a9e5d72a37202
david.c.liebman@gmail.com <david.c.liebman@gmail.com@9563fa94-8a37-d0f4-0ffc-d383d6e30373>
2013-01-10 19:13:19 +0000

    change to https

--- src/org/davidliebman/android/awesomeguy/WebScoreUpload.java
+++ src/org/davidliebman/android/awesomeguy/WebScoreUpload.java
@@ -21,7 +21,7 @@ import com.google.gson.Gson;
 public class WebScoreUpload {
 
 	//public static final String MY_URL = new String("http://10.0.2.2:8888");
-	public static final String MY_URL = new String("http://awesomescores.appspot.com");
+	public static final String MY_URL = new String("https://awesomescores.appspot.com");
 	public static final String MY_PATH_GAME = new String ("/game.html");
 	public static final String MY_PATH_INDEX = new String ("/index.html");
 	
