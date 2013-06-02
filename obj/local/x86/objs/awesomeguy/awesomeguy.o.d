200b7490928996d919d50651726c95df52f86788
david.c.liebman <david.c.liebman@9563fa94-8a37-d0f4-0ffc-d383d6e30373>
2013-06-02 17:36:34 +0000

    adding support for other arch's

---
+++ obj/local/x86/objs/awesomeguy/awesomeguy.o.d
@@ -0,0 +1,145 @@
+obj/local/x86/objs/awesomeguy/awesomeguy.o: jni/awesomeguy.c \
+ jni/androidgl.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/jni.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/android/log.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/stdio.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/stdint.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/stdlib.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/string.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/malloc.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/alloca.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/strings.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/memory.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/math.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/limits.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/assert.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/pthread.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/time.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/time.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/time.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/siginfo.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/signal.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/signal.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm-generic/signal.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/sigcontext.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sched.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/GLES/gl.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/GLES/glplatform.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/KHR/khrplatform.h \
+ /home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/GLES/glext.h
+
+jni/androidgl.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/jni.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/android/log.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/stdio.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/stdlib.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/string.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/alloca.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/strings.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/memory.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/math.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/limits.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/assert.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/pthread.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/time.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sys/time.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/linux/time.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/siginfo.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/signal.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/signal.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm-generic/signal.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/asm/sigcontext.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/sched.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/GLES/gl.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/GLES/glplatform.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/KHR/khrplatform.h:
+
+/home/dave/bin/android-ndk-r8/platforms/android-9/arch-x86/usr/include/GLES/glext.h:
