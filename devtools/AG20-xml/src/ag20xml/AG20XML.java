557865d0c295e8b2a01d5d5eaf51fc0a5d5ecd3d
david.c.liebman <david.c.liebman@9563fa94-8a37-d0f4-0ffc-d383d6e30373>
2013-07-29 23:03:39 +0000

    change size of text field

---
+++ devtools/AG20-xml/src/ag20xml/AG20XML.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ag20xml;
+
+/**
+ *
+ * @author dave
+ */
+public class AG20XML {
+    
+}
